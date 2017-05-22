b7330037b9432ca76db594aee72cec4022838190
Bryan Charlie <bryancharlie@Bryans-MacBook-Pro.local>
2017-05-22 15:04:52 -0400

    Initial fetch API middleware setup.

--- index.ios.js
+++ index.ios.js
@@ -1,5 +1,7 @@
 const containers = './src/cex/containers'
 
+//import 'config/reactotron'
+
 import React from 'react'
 import { AppRegistry } from 'react-native'
 import Navigation from 'containers/Navigation'
