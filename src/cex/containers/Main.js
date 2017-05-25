2c2a3bde01fae286cf7b17b18448755291791dd4
Bryan Charlie <bryancharlie@Bryans-MacBook-Pro.local>
2017-05-25 11:28:23 -0400

    Tentative login skip.

--- src/cex/containers/Main.js
+++ src/cex/containers/Main.js
@@ -7,28 +7,16 @@ import React, { Component } from 'react'
 import { Provider } from 'react-redux'
 import { configureStore } from 'store/configureStore'
 import { MainScreen } from './MainScreen'
-import { getUser } from 'lib/http'
 
 const store = configureStore()
 
 console.log(store.getState())
 
 export default class Navigation extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      isFetching: true
-    }
-  }
-
-  componentWillMount = () => {
-
-  }
-
   render(){
     return(
       <Provider store={store}>
-        <MainScreen />
+        <MainScreen/>
       </Provider>
     )
   }
