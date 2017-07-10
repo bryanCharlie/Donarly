764519a3c6ed21387d903fe90abe6ddfdbfe577e
Bryan Charlie <bcharlie1886@gmail.com>
2017-07-09 20:17:59 -0400

    Added profile methods

--- src/config/dev.js
+++ src/config/dev.js
@@ -4,7 +4,7 @@ let config = {
     user_key: 'charity_user',
     API_AUTH_ENDPOINT: `${API_ENDPOINT_BASE}/auth`,
     API_DONATION_ENDPOINT: `${API_ENDPOINT_BASE}/donations`,
-    API_PROFILE_ENDPOINT: `${API_ENDPOINT_BASE}/users`,
+    API_USERS_ENDPOINT: `${API_ENDPOINT_BASE}/users`,
     API_TRANSACTION_ENDPOINT: `${API_ENDPOINT_BASE}/transactions`
 };
 
