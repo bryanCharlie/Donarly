32232d8806648297c0df40913bf87f0f576bf3e7
Dianne Robbi <dianne.dannang@rocketmail.com>
2017-11-10 12:06:49 -0500

    Updated ImageButton

--- src/cex/components/assets/ImageButton.js
+++ src/cex/components/assets/ImageButton.js
@@ -1,12 +1,44 @@
 import React, { Component } from 'react';
-import { View, TouchableOpacity, KeyboardAvoidingView, ScrollView, Image, StyleSheet } from 'react-native';
+import { View, TouchableOpacity, KeyboardAvoidingView, ScrollView, Image } from 'react-native';
+import { Button } from 'assets/button';
 
 export const ImageButton = ({onButtonPress, orgImageURI}) => {
     return(
         <TouchableOpacity onPress= { onButtonPress }>
             <View style = {style}>
-                <Image source={{uri: orgImageURI}} style={style.image}/>
+                <Image source={{uri: orgImageURI}} style={style}/>
             </View>
         </TouchableOpacity>
     );
 };
+
+const style = {
+    height: 150, 
+    width: 200
+};
+
+/**
+export class ImageButton extends Component {
+    constructor(props){
+        super(props);
+        
+        const imgUri = this.props.orgImageURI;
+    }
+    
+    render(){
+        return(
+            <Button>
+            <TouchableOpacity onPress= { onButtonPress }>
+                <View>
+                    <Image source={{uri: imgUri}} style={style}/>
+                </View>
+            </TouchableOpacity>
+            </Button>
+        );
+    }
+}
+
+const style = {
+    height: 150, 
+    width: 200
+}; */
