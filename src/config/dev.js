5c7e52a456e4baf910021ef284f5e4d8db665791
Bryan Charlie <bcharlie1886@gmail.com>
2018-01-07 13:34:36 -0500

    fixed endpoint error

--- src/config/dev.js
+++ src/config/dev.js
@@ -1,4 +1,4 @@
-const API_ENDPOINT_BASE = 'https://flask-json-api.herokuapp.com/';
+const API_ENDPOINT_BASE = 'https://flask-json-api.herokuapp.com';
 const CC_PUBLIC_KEY = 'pk_test_hoqO8vgA9RK-VSXr6gCU1Q';
 
 let config = {
